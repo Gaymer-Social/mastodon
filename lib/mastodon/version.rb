e1acefb593a6826a85b680400919dcfd0ca61193
Stuxie.Dev <contact@stuxie.dev>
2022-11-29 12:24:29 +0000

    Update version.rb

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      '+glitch'
+      '+gaymer'
     end
 
     def to_a
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'glitch-soc/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'GaymerSocial/mastodon')
     end
 
     def source_base_url
